c158880e2081d661350841a00f01e1ba5146f380
Chris <messiercr@gmail.com>
2018-10-30 16:51:55 -0400

    ran files through 2to3

--- process_data/commands.sh
+++ process_data/commands.sh
@@ -13,7 +13,7 @@ done
 ./process_libsvm_datasets.py usps usps usps.t
 ./process_libsvm_datasets.py dna dna.scale.tr dna.scale.t   # ignoring validation files
 ./process_libsvm_datasets.py satimage satimage.scale.tr satimage.scale.t    # ignoring validation files
-# ./process_libsvm_datasets.py letter letter.scale.tr letter.scale.t  # ignoring validation files
+./process_libsvm_datasets.py letter letter.scale.tr letter.scale.t  # ignoring validation files
 ./process_libsvm_datasets.py letter letter.scale.tr letter.scale.t letter.scale.val # adding validation to training (to make comparisons with OnlineRF by Saffari et al.)
 cd dna-61-120
 ./process_dna-61-120.py
--- process_data/dna-61-120/process_dna-61-120.py
+++ process_data/dna-61-120/process_dna-61-120.py
@@ -3,10 +3,10 @@
 # script to extract dimensions 61-120 of the dna dataset
 # According to https://www.sgi.com/tech/mlc/db/DNA.names,
 # " Hint.   Much better performance is generally observed if attributes
-#           closest to the junction are used. In the StatLog version, 
+#           closest to the junction are used. In the StatLog version,
 #           this means using attributes A61 to A120 only."
 
-import cPickle as pickle
+import pickle
 import sys
 
 data = pickle.load(open('../dna/dna.p', 'rb'))
